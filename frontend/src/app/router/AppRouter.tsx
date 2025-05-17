e22b3248c98e7a66b916ca025fc6c57b047377e6
myloyo <mylnikova2334@mail.ru>
2025-05-17 19:31:36 +0400

    it works!!!!!

--- frontend/src/app/router/AppRouter.tsx
+++ frontend/src/app/router/AppRouter.tsx
@@ -2,27 +2,30 @@ import { ActivatePage } from '@/pages/AuthPages/ActivatePage/ActivatePage'
 import { LoginPage } from '@/pages/AuthPages/LoginPage/LoginPage'
 import { RegisterPage } from '@/pages/AuthPages/RegisterPage/RegisterPage'
 import { MainPage } from '@/pages/MainPage/MainPage'
+import { StatsPage } from '@/pages/StatsPage/StatsPage';
 import { Outlet, Route, Routes } from 'react-router-dom'
 import { MainLayout } from '../layouts/MainLayout'
 
 export const Router = () => {
-	return (
-		<Routes>
-			<Route path='/login' element={<LoginPage />} />
-			<Route path='/register' element={<RegisterPage />} />
-			<Route path='/activate-account' element={<ActivatePage />} />
-
-			<Route element={<MainLayoutWrapper />}>
-				<Route path='/' element={<MainPage />} />
-			</Route>
-		</Routes>
-	)
+  return (
+    <Routes>
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+      <Route path='/activate-account' element={<ActivatePage />} />
+      
+      {/* Все роуты внутри MainLayoutWrapper получат общий лейаут */}
+      <Route element={<MainLayoutWrapper />}>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/stats' element={<StatsPage />} />
+      </Route>
+    </Routes>
+  )
 }
 
 const MainLayoutWrapper = () => {
-	return (
-		<MainLayout>
-			<Outlet />
-		</MainLayout>
-	)
+  return (
+    <MainLayout>
+      <Outlet />
+    </MainLayout>
+  )
 }
